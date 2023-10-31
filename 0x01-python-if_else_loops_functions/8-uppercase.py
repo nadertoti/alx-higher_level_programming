b730fb43b33293423f194d8ddb9ada60a74375a6
nadertoti <nadertoti4@gmail.com>
2023-10-31 13:13:49 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3                                                       
-def islower(c):
+#!/usr/bin/python3                                                      def islower(c):
     if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
