492fe4f4c6e702c8f978b0a090c92cc472fc0789
nadertoti <nadertoti4@gmail.com>
2023-11-21 03:02:41 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3                                                               
+#!/usr/bin/python3
+
 """Square module. """
 
 
@@ -6,17 +7,13 @@ class Square:
     """Defines a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Constructor.
-        
-        """
+        """Constructor."""
         self.__size = size
-        self.position = position
-        
+        self.__position = position  # Use a different name for the instance attribute
+
     @property
     def size(self):
-        """property for the length of a side of this square.
-        
-        """
+        """Property for the length of a side of this square."""
         return self.__size
 
     @size.setter
@@ -29,10 +26,8 @@ class Square:
 
     @property
     def position(self):
-        """Area of the square.
-        
-        """
-        return (self.__position)
+        """Position of the square."""
+        return self.__position
 
     @position.setter
     def position(self, value):
@@ -41,19 +36,28 @@ class Square:
                 not all(isinstance(number, int) for number in value) or
                 not all(number >= 0 for number in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.position = value
+        self.__position = value  # Use a different name for the instance attribute
+
     def area(self):
-        """return."""
-        return (self.__size * self.__size)
-    
+        """Return the area of the square."""
+        return self.__size * self.__size
+
     def my_print(self):
-        """prints the square."""
+        """Print the square."""
         if self.__size == 0:
             print("")
             return
 
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="" for k in range(0, self.__size)]
+        for i in range(0, self.__position[1]):
             print("")
+
+        for i in range(0, self.__size):
+            for j in range(0, self.__position[0]):
+                print(" ", end="")
+            for k in range(0, self.__size):
+                print("#", end="")
+            print("")
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    my_square_1.my_print()
