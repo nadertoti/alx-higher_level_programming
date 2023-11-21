ee4d3019af7637e77a46ca05de46afbde06d2c98
nadertoti <nadertoti4@gmail.com>
2023-11-21 02:02:12 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" """
+"""Square module."""
 
 class Square:
     def __init__(self, size=0):
-        """Constroctor. """
+        """Constroctor.
+        
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
     def area(self):
-        """Area """
+        """Area."""
         return self.__size ** 2
