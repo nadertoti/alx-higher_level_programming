04c84d66718b3a40bcc561b05aa5a360101d7363
nadertoti <nadertoti4@gmail.com>
2023-12-04 09:25:23 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for lookup method."""
+class MyList(list):
+    """Custom the list."""
+    def print_sorted(self):
+        """Approuch for print sorted list."""
+        print(sorted(self))
