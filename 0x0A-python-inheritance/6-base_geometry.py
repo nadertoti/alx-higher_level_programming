06db1a6525b97d28c9cdf69db9428daee46fccfd
nadertoti <nadertoti4@gmail.com>
2023-12-04 11:19:50 -0800

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for  basegeometry class method."""
+
+
+class BaseGeometry:
+    """A BaseGeometry class."""
+    def area(self):
+        """Compute this area."""
+        raise Exception("area() is not implemented")
