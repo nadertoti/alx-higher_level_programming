4d186b99c76de1c311ccda619b0e5ec7ead73f75
nadertoti <nadertoti4@gmail.com>
2023-12-04 10:01:08 -0800

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for 2 is same class method."""
+
+
+def is_same_class(obj, a_class):
+    """Combier if an object is exactly an instance of a class."""
+    return type(obj) == a_class
