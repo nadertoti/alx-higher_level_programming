e014696edeebbfe9b349486e82ecf77f988f752c
nadertoti <nadertoti4@gmail.com>
2023-12-04 10:31:09 -0800

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for 2 is same class method."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Determine if an object is a subclass."""
+    return isinstance(obj, a_class)
