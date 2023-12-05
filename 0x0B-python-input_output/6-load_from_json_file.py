29d6682ee9838e5ebba43d4d42eabd40cbd521e9
nadertoti <nadertoti4@gmail.com>
2023-12-05 08:04:14 -0800

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """creates an object from jsom file"""
     with open(filename, 'r', encoding="utf-8") as f:
-        return json.loads(f)
+        return json.load(f)
