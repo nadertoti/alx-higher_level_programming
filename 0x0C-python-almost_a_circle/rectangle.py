a1f87ff7bfdde5f99f5a8eea6b161fc72e7fb3ea
nadertoti <nadertoti4@gmail.com>
2023-12-11 09:53:45 -0800

    done

---
+++ 0x0C-python-almost_a_circle/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Module for rectangle class."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A rectangle class."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constroctor."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """height of the rectangle."""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """height of the rectangle."""
+        self.__width = value
+    
+    @property
+    def height(self):
+        """height of the rectangle."""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """height of the rectangle."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """height of the rectangle."""
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """height of the rectangle."""
+        self.__x = value
+    
+    @property
+    def y(self):
+        """height of the rectangle."""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
