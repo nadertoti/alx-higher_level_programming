03fc7d7761d9fcc225757dd9a08fce1c083c825c
nadertoti <nadertoti4@gmail.com>
2024-01-16 14:13:34 -0800

    done

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- script that inserts a new row in the table first_table
+-- add value
+-- excecute command sql to do that query
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
