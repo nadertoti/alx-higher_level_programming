b09b690b20d4f32458eb86f33d084ca4b500aeb2
nadertoti <nadertoti4@gmail.com>
2024-01-17 12:33:42 -0800

    done

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- list all table in db
+SELECT
+tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY 2 DESC;
